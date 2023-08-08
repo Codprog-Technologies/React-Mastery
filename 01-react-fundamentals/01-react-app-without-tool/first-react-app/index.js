b93db894fa8b0a3f70bd8e46da513e660494caa2
harshit-codprog <harshit.codprog@gmail.com>
2023-08-08 16:36:29 +0530

    updated code

--- 01-react-fundamentals/01-react-app-without-tool/first-react-app/index.js
+++ 01-react-fundamentals/01-react-app-without-tool/first-react-app/index.js
@@ -1,38 +1,14 @@
 const rootElement = document.getElementById("root");
 
-// console.log("Hello world");
+// // type, props, children
+// // function HelloWorld() {
+// //     return React.createElement("h1", { className: "heading" }, "Hello World");
+// // }
 
-//
-// 1. type of element h1, h2, div, form, select, main
-// 2. properties / props / configurations , null, {}
-// 3. children
-
-// const h1Element = React.createElement(
-//     "h1",
-//     { className: "heading" },
-//     "React App"
-// ); // this will create elements
-
-// shortcut
-// jsx : javascript xml
-
-// const mainHeading = <h1 className="heading">React App</h1>;
-
-// component (functional component)
-
-// first letter should always be capital
-
-function MainHeading() {
-    return (
-        <div className="heading">
-            <h1>React App 1</h1>
-            <p>this is para</p>
-        </div>
-    );
+function HelloWorld() {
+    return <h1 className="heading">Hello World</h1>;
 }
 
-// ReactDOM.createRoot(rootElement).render(mainHeading());
-ReactDOM.createRoot(rootElement).render(<MainHeading />);
+// h1, h2, h3, h4, div, p , article, main
 
-// jsx
-// react.createElement
+ReactDOM.createRoot(rootElement).render(<HelloWorld />);
