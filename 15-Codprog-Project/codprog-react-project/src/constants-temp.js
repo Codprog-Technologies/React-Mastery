1b8c88ba80317229eb959f9a1202bd6a9d62fa19
Harshit Vashisth <harshit@Harshits-MacBook-Air.local>
2024-01-08 19:13:52 +0530

    add constants to readme file

--- 15-Codprog-Project/codprog-react-project/src/constants-temp.js
+++ 15-Codprog-Project/codprog-react-project/src/constants-temp.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://orfqhbdeqtusogzodofb.supabase.co/auth/v1/";
+export const BASE_URL = "https://<yourprojectid>.supabase.co/auth/v1/";
 export const LOGIN_URL = BASE_URL + "token?grant_type=password";
 export const SUPBASE_API_KEY = "Your supabase api key";
--- 15-Codprog-Project/codprog-react-project/src/constants.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = "https://orfqhbdeqtusogzodofb.supabase.co/auth/v1/";
-export const LOGIN_URL = BASE_URL + "token?grant_type=password";
-export const SUPBASE_API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yZnFoYmRlcXR1c29nem9kb2ZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIyMDM5MzQsImV4cCI6MjAxNzc3OTkzNH0.lPpNrQwmEyVK2hcmH24-w0m78yRQjvqMlj2tK73tThw";
